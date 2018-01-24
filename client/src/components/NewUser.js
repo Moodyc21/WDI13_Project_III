258f40571fde495545f1ec614e55c35d50207d47
Christopher Moody <moodyc21@gmail.com>
2018-01-24 13:14:21 -0500

    added styled component to New User page

--- client/src/components/NewUser.js
+++ client/src/components/NewUser.js
@@ -1,37 +1,73 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const FormWrapper = styled.div`
+        background-color: #0E3F8A;
+        h1 {
+            margin: 0 auto;
+            text-align: center;
+            color: white;
+}
+        label {
+            color: white;
+        }
+
+`
+
+const FormColumn = styled.div`
+    
+`
+const LabelStyle = styled.div`
+        display: flex;
+        justify-content: center;
+        padding: 15px;
+
+`
 
 class NewUser extends Component {
     render () {
         return (
-            <div>
+            <FormWrapper>
                 <h1>Create a New User</h1>
-                <div>
+                <FormColumn>
                     <form>
+                        <LabelStyle>
                         <label>
                             First Name
                             <input type="text"/>
                         </label>
+                        </LabelStyle>
+                        <LabelStyle>
                         <label>
                             Last Name
                             <input type="text"/>
                         </label>
+                        </LabelStyle>
+                        <LabelStyle>
                         <label>
                             Email
                             <input type="text"/>
                         </label>
+                        </LabelStyle>
+                        <LabelStyle>
                         <label>
                             Handicap
                             <input type="text"/>
                         </label>
+                        </LabelStyle>
+                        <LabelStyle>
                         <label>
                             PhotoURL
                             <input type="text"/>
                         </label>
+                        </LabelStyle>
+                        <LabelStyle>
                         <input type='submit' value='Submit'/>
+                        </LabelStyle>
                     </form>
-                </div>
-            </div>
+                </FormColumn>
+            </FormWrapper>
             
         )
     }
