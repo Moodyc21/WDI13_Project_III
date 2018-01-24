7c81cec9188ecae18c7fa61ecdb8c3fda3fb688e
Christopher Moody <moodyc21@gmail.com>
2018-01-24 14:06:10 -0500

    added styled components to New User page

--- client/src/components/NewUser.js
+++ client/src/components/NewUser.js
@@ -5,11 +5,13 @@ import styled from 'styled-components'
 const FormWrapper = styled.div`
         background-color: #0E3F8A;
         h1 {
+            font-size: 80px;
             margin: 0 auto;
             text-align: center;
             color: white;
 }
         label {
+            font-size: 25px;
             color: white;
         }
 
@@ -21,49 +23,73 @@ const FormColumn = styled.div`
 const LabelStyle = styled.div`
         display: flex;
         justify-content: center;
-        padding: 15px;
+        padding: 12px;
+        [type=text] {
+            border-radius: 3px;
+        }
+        
 
+`
+const SubmitButton = styled.div`
+        padding: 10px;
+        [type=submit] {
+            color: white;
+            background-color: rgb(200, 0, 0);
+            border: 1px solid white;
+            border-radius: 5px;
+            padding: 3px 20px;
+            }
+            [type=submit]:hover {
+                background-color: darkred;
+            }
 `
 
 class NewUser extends Component {
     render () {
         return (
             <FormWrapper>
-                <h1>Create a New User</h1>
+                <h1>Create <br/> User</h1>
                 <FormColumn>
-                    <form>
+                    <form >
                         <LabelStyle>
                         <label>
-                            First Name
-                            <input type="text"/>
+                            {/* First Name */}
+                            
+                            <input type="text" placeholder='First Name'/>
                         </label>
                         </LabelStyle>
                         <LabelStyle>
                         <label>
-                            Last Name
-                            <input type="text"/>
+                            {/* Last Name */}
+                            
+                            <input type="text" placeholder='Last Name'/>
                         </label>
                         </LabelStyle>
                         <LabelStyle>
                         <label>
-                            Email
-                            <input type="text"/>
+                            {/* Email */}
+                           
+                            <input type="text" placeholder='Email'/>
                         </label>
                         </LabelStyle>
                         <LabelStyle>
                         <label>
-                            Handicap
-                            <input type="text"/>
+                            {/* Handicap */}
+                            
+                            <input type="text" placeholder='Handicap'/>
                         </label>
                         </LabelStyle>
                         <LabelStyle>
                         <label>
-                            PhotoURL
-                            <input type="text"/>
+                            {/* PhotoURL */}
+                            
+                            <input type="text" placeholder='PhotoURL'/>
                         </label>
                         </LabelStyle>
                         <LabelStyle>
+                            <SubmitButton>
                         <input type='submit' value='Submit'/>
+                        </SubmitButton>
                         </LabelStyle>
                     </form>
                 </FormColumn>
